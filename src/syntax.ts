6810573434d0f46b4aed3beb66c579f0cefe0d7b
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2021-02-13 16:38:57 +0900

    Add syntax

---
+++ src/syntax.ts
@@ -0,0 +1,155 @@
+function indent(x: Block):string {
+    let lines = x.toString()
+    if (lines.endsWith("\n")) {
+        lines = lines.substr(0, lines.length - 1)
+    }
+    return lines.split("\n").map(line => `  ${line}`).join("\n")
+}
+
+
+export interface Block {
+    toString(): string
+}
+
+export class TypeIdentifier implements Block {
+    constructor(public readonly id: string) {}
+    toString(): string { return `#${this.id}` }
+}
+export class PolymorphicType implements Block {
+    constructor(
+        public readonly id: TypeIdentifier,
+        public readonly typevars: ReadonlyArray<Type>,
+    ) {}
+    toString(): string { return `${this.id}<${this.typevars.join(",")}>` }
+}
+export class TupleType implements Block {
+    constructor(public readonly types: ReadonlyArray<Type>) {}
+    toString(): string { return `#(${this.types.join(",")})` }
+}
+export type Type = TypeIdentifier| PolymorphicType | TupleType
+
+export class Num implements Block {
+    constructor(public readonly value: string) {}
+    toString(): string { return this.value }
+}
+export class Str implements Block {
+    constructor(public readonly value: string) {}
+    toString(): string { return `"${this.value}"` }
+}
+export type Primitive = Num | Str
+
+export class Identifier implements Block {
+    constructor(public readonly id: string) {}
+    toString(): string { return `$${this.id}` }
+}
+export class Declaration implements Block {
+    constructor(
+        public readonly arg: Identifier,
+        public readonly type: Type,
+    ) {}
+    toString(): string { return `${this.arg}:${this.type}` }
+}
+export class Tuple implements Block {
+    constructor(public readonly types: ReadonlyArray<Expression>) {}
+    toString(): string { return `(${this.types.join(",")})` }
+}
+export class Func implements Block {
+    constructor(
+        public readonly decls: ReadonlyArray<Declaration>,
+        public readonly returnType: Type,
+        public readonly body: Statement,
+    ) {}
+    toString(): string {
+        return `(${this.decls.join(",")})->${this.returnType}{\n${indent(this.body)}\n}`
+    }
+}
+export class BinOp implements Block {
+    constructor(
+        public readonly lhs: Expression,
+        public readonly op: Identifier,
+        public readonly rhs: Expression,
+    ) {}
+    toString(): string { return `(${this.lhs}${this.op}${this.rhs})`}
+}
+export class Call implements Block {
+    constructor(
+        public readonly func: Identifier,
+        public readonly arg: Expression,
+    ) {}
+    toString(): string { return `(${this.func}(${this.arg}))` }
+}
+export type Expression = Primitive | Identifier | Tuple | Func | BinOp | Call
+
+export class Assign implements Block {
+    constructor(
+        public readonly lhs: Identifier,
+        public readonly is_define: boolean,
+        public readonly rhs: Expression,
+    ) {}
+    toString(): string {
+        if (this.is_define) {
+            return `${this.lhs}:=${this.rhs}\n`
+        } else {
+            return `${this.lhs}<-${this.rhs}\n`
+        }
+    }
+}
+export class Do implements Block {
+    constructor(public readonly expr: Expression) {}
+    toString(): string { return `do(${this.expr})\n`}
+}
+export class Loop implements Block {
+    constructor(
+        public readonly id: Identifier,
+        public readonly iterable: Expression,
+        public readonly body: Statement,
+    ) {}
+    toString(): string {
+        return `foreach(${this.id}<-${this.iterable}){\n${indent(this.body)}\n}\n`
+    }
+}
+export class Branch implements Block {
+    constructor(
+        public readonly cases: ReadonlyArray<Case>,
+        public readonly _default: Default | null,
+    ) {}
+    toString() {
+        if (this._default) {
+            return `branch{\n${this.cases.join("")}${this._default}}\n`
+        } else {
+            return `branch{\n${this.cases.join("")}}\n`
+        }
+    }
+}
+export class Case implements Block {
+    constructor(
+        public readonly cond: Expression,
+        public readonly body: Statement,
+    ) {}
+    toString() { return `case(${this.cond}):\n${indent(this.body)}\n` }
+}
+export class Default implements Block {
+    constructor(public readonly body: Statement) {}
+    toString() { return `default:\n${indent(this.body)}\n` }
+}
+export class Return implements Block {
+    constructor(public readonly value: Expression | null) {}
+    toString() {
+        if (this.value) {
+            return `return(${this.value})\n`
+        } else {
+            return "return\n"
+        }
+    }
+}
+export class Break implements Block {
+    toString() { return "break\n" }
+}
+export class Continue implements Block {
+    toString() { return "continue\n" }
+}
+export class Suite implements Block {
+    constructor(public readonly types: ReadonlyArray<Statement>) {}
+    toString(): string { return `${this.types.join("")}` }
+}
+export type Statement = Assign | Do | Loop | Branch | Return | Break | Continue | Suite
---
+++ test/test_syntax.ts
@@ -0,0 +1,147 @@
+import * as chai from 'chai'
+chai.should()
+
+import * as $ from "../src/syntax"
+
+describe("toString", () => {
+    it("TypeIdentifier", () => {
+        new $.TypeIdentifier("int").toString().should.equal("#int")
+    })
+    it("PolymorphicType", () => {
+        new $.PolymorphicType(
+            new $.TypeIdentifier("map"),
+            [new $.TypeIdentifier("int"), new $.TypeIdentifier("string")],
+        ).toString().should.deep.equal("#map<#int,#string>")
+    })
+    it("TupleType", () => {
+        new $.TupleType(
+            [new $.TypeIdentifier("x"), new $.TypeIdentifier("y")]
+        ).toString().should.deep.equal("#(#x,#y)")
+    })
+
+    it("Num", () => {
+        new $.Num("10").toString().should.deep.equal("10")
+    })
+    it("Str", () => {
+        new $.Str("10").toString().should.deep.equal("\"10\"")
+    })
+    it("Identifier", () => {
+        new $.Identifier("x").toString().should.deep.equal("$x")
+    })
+    it("Tuple", () => {
+        new $.Tuple(
+            [new $.Identifier("x"), new $.Identifier("y")]
+        ).toString().should.deep.equal("($x,$y)")
+    })
+    describe("Func", () => {
+        it("Declaration", () => {
+            new $.Declaration(
+                new $.Identifier("x"), new $.TypeIdentifier("y")
+            ).toString().should.deep.equal("$x:#y")
+        })
+        it("Func",() => {
+            new $.Func(
+                [
+                    new $.Declaration(
+                        new $.Identifier("x"),
+                        new $.TypeIdentifier("y"),
+                    ),
+                    new $.Declaration(
+                        new $.Identifier("z"),
+                        new $.TypeIdentifier("w"),
+                    ),
+                ],
+                new $.TypeIdentifier("r"),
+                new $.Do(new $.Identifier("p"))
+            ).toString().should.deep.equal(
+                "($x:#y,$z:#w)->#r{\n  do($p)\n}"
+            )
+        })
+    })
+    it("BinOp", () => {
+        new $.BinOp(
+            new $.Identifier("x"), new $.Identifier("+"), new $.Identifier("y")
+        ).toString().should.deep.equal("($x$+$y)")
+    })
+    it("Call", () => {
+        new $.Call(
+            new $.Identifier("f"), new $.Identifier("x")
+        ).toString().should.deep.equal("($f($x))")
+    })
+
+    describe("Assign", () => {
+        it("define",() => {
+            new $.Assign(
+                new $.Identifier("x"), true, new $.Identifier("y")
+            ).toString().should.deep.equal("$x:=$y\n")
+        })
+        it("assign",() => {
+            new $.Assign(
+                new $.Identifier("x"), false, new $.Identifier("y")
+            ).toString().should.deep.equal("$x<-$y\n")
+        })
+    })
+    it("Do", () => {
+        new $.Do(new $.Identifier("x")).toString().should.deep.equal("do($x)\n")
+    })
+    it("Loop", () => {
+        new $.Loop(
+            new $.Identifier("x"), new $.Identifier("xs"),
+            new $.Do(new $.Identifier("z")),
+        ).toString().should.deep.equal("foreach($x<-$xs){\n  do($z)\n}\n")
+    })
+    describe("Branch", () => {
+        it("Case",() => {
+            new $.Case(
+                new $.Identifier("cond"), new $.Do(new $.Identifier("x"))
+            ).toString().should.deep.equal("case($cond):\n  do($x)\n")
+        })
+        it("Default",() => {
+            new $.Default(
+                new $.Do(new $.Identifier("x"))
+            ).toString().should.deep.equal("default:\n  do($x)\n")
+        })
+        it("Branch", () => {
+            const _case0 = new $.Case(
+                new $.Identifier("cond0"), new $.Do(new $.Identifier("x"))
+            )
+            const _case1 = new $.Case(
+                new $.Identifier("cond1"), new $.Do(new $.Identifier("y"))
+            )
+            new $.Branch(
+                [_case0, _case1], null
+            ).toString().should.deep.equal("branch{\ncase($cond0):\n  do($x)\ncase($cond1):\n  do($y)\n}\n")
+        })
+        it("BranchWithDefault", () => {
+            const _case0 = new $.Case(
+                new $.Identifier("cond0"), new $.Do(new $.Identifier("x"))
+            )
+            const _case1 = new $.Case(
+                new $.Identifier("cond1"), new $.Do(new $.Identifier("y"))
+            )
+            const _default = new $.Default(new $.Do(new $.Identifier("z")))
+            new $.Branch(
+                [_case0, _case1], _default
+            ).toString().should.deep.equal("branch{\ncase($cond0):\n  do($x)\ncase($cond1):\n  do($y)\ndefault:\n  do($z)\n}\n")
+        })
+    })
+    describe("Return", () => {
+        it("WithValue", () => {
+            new $.Return(new $.Identifier("x")).toString().should.deep.equal("return($x)\n")
+        })
+        it("WithoutValue", () => {
+            new $.Return(null).toString().should.deep.equal("return\n")
+        })
+    })
+    it("Break", () => {
+        new $.Break().toString().should.deep.equal("break\n")
+    })
+    it("Continue", () => {
+        new $.Continue().toString().should.deep.equal("continue\n")
+    })
+    it("Suite", () => {
+        new $.Suite(
+            [new $.Do(new $.Identifier("x")), new $.Return(null)]
+        ).toString().should.deep.equal("do($x)\nreturn\n")
+    })
+})
